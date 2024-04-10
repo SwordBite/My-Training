3a3a016cb5a85b624573c01d2c6dde992e77fc79
SwordBite <gmr.crowley@gmail.com>
2024-04-10 23:26:04 +0300

    Update

--- Arrays/find-max.js
+++ Arrays/find-max.js
@@ -1,6 +1,8 @@
-function findMax() {
-    const numbers = [20, 56, 201, 90, 45];
-    let result = Math.max.apply(null, numbers);
-    console.log(result);
-}
+// function findMax() {
+//     const numbers = [20, 56, 201, 90, 45];
+//     let result = Math.max.apply(null, numbers);
+//     console.log(result);
+// }
+
+// Самостоятельно написать поиск максиального числа в массиве
 findMax()
--- Arrays/remove-from-array.js
+++ Arrays/remove-from-array.js
@@ -3,4 +3,8 @@ function removeFromArray() {
     const removeByIndex = myArray.splice(0, 1);
     console.log(myArray);
 }
+
+
+// Написать новую функцию, которая принимает в себя 1 входной параметр элемент массива, который нужно удалить из массива (не индекс)
+
 removeFromArray();
